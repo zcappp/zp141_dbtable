d80c296049c321685ffb254a265f905290e256ab
众触应用平台 <87555332+zcappp@users.noreply.github.com>
2024-02-19 09:08:33 +0800

    1

--- src/zp141_数据库表格.js
+++ src/zp141_数据库表格.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react"
-import css from "./zp141_数据库查询表格.css"
+import css from "./zp141_数据库表格.css"
 
 const ValidOID = new RegExp("^[0-9a-pA-p]{24}$")
 const _id = "_id"
@@ -13,15 +13,16 @@ function init(ref) {
     ref.sorts = []
     ref.sortR = [] // 倒序
     const p = ref.props
-    if (p.path) ref.DPath = p.path.startsWith("$c.x") ? p.path : "$c.x." + p.path
-    ref.container.export = () => download(ref)
+    if (p.path) ref.ppath = p.path.startsWith("$c.x") ? p.path : "$c.x." + p.path
     if (p.loadMore) loadMore(ref)
-    _doList(ref)
+    ref.container.export = () => download(ref)
+    ref.container.edit = $x => toEdit(ref, $x)
+    init1(ref)
 }
 
 function render(ref) {
-    if (!ref.DPath) return <div>请配置数据路径</div>
-    ref.D = excA(ref.DPath)
+    if (!ref.ppath) return <div>请配置数据路径</div>
+    ref.D = excA(ref.ppath)
     if (!ref.D) return <div/>
     ref.list = ref.props.loadMore ? ref.D.all : ref.D.arr
     if (!ref.Q0) {
@@ -31,7 +32,7 @@ function render(ref) {
         if (typeof ref.O.sort == "string") ref.O.sort.split(" ").forEach(a => {
             if (a) a.startsWith("-") ? ref.sortR.push(a) : ref.sorts.push(a)
         })
-        _doList(ref)
+        init1(ref)
     }
     ref.doList()
     return <Fragment>{rTop(ref)}<div className="main">{!!ref.tree && rTree(ref)}{rTable(ref)}</div>
@@ -57,7 +58,7 @@ function rTable(ref) {
 function rTree(ref) {
     return <div className="treeWrap">
         <div className="treeHead">{ref.tree.heads.join(" | ")}</div>
-        <div className="tree scrollbar">{rTree1(ref, ref.tree.root, ref.tree.fields.length - 1, [])}</div>
+        <div className="tree scrollbar">{rTree1(ref, ref.tree.root.concat(["null"]), ref.tree.fields.length - 1, [])}</div>
     </div>
 }
 
@@ -67,7 +68,7 @@ function rTree1(ref, root, len, parent) {
         return <div className="node" data-path={path} key={i}>
             <span onClick={e => switcher(ref, e)} className={"switcher" + (len > parent.length ? " hasChild" : "") + (ref.tree.open[path] ? "" : " close")}/>
             <span onClick={e => selNode(ref, e)} className={"txt" + (ref.tree.sel == path ? " selected" : "")}>{a + "" || "空白"}</span>
-            {ref.tree.open[path] ? rTree1(ref, ref.tree[path], len, parent.concat([i])) : ""}
+            {ref.tree.open[path] ? rTree1(ref, ref.tree[path].concat(["null"]), len, parent.concat([i])) : ""}
         </div>
     })
 }
@@ -81,10 +82,11 @@ function rTop(ref) {
             <svg onClick={() => setDay(ref)} viewBox="0 0 1024 1024" className="rminput zsvg clock"><path d="M533.312 556.416V219.52H438.848v392.32h1.472l243.84 140.8 47.296-81.728-198.144-114.432zM511.488 0C794.624 0 1024 229.376 1024 512s-229.376 512-512.512 512C228.864 1024 0 794.624 0 512s228.864-512 511.488-512z"></path></svg>
             <svg onClick={() => {ref.Q = {}; search(ref); ref.render()}} viewBox="64 64 896 896" className="rminput zsvg"><path d={RmInput}></path></svg>
         </span>}
-        {p.searchFieldOn && p.searchFields && p.searchFields.map((o, i) => searchField(ref, o, i))}
-        {(p.searchBox || p.searchFieldOn) && <button onClick={() => searchBtn(ref)} className="zbtn search">查询</button>}
+        {p.enableSearchField && p.searchFields && p.searchFields.map((o, i) => searchField(ref, o, i))}
+        {(p.searchBox || p.enableSearchField) && <button onClick={() => searchBtn(ref)} className="zbtn search">查询</button>}
         {!!p.loaded && <span className="loaded">已加载/总数：<strong>{ref.D.skip + ref.D.arr.length}</strong>/<strong>{ref.D.count}</strong></span>}
         {!!p.Excel && <span onClick={() => download(ref)} className="zbtn export">导出</span>}
+        {p.onNew && p.editPop && <span onClick={() => toEdit(ref)} className="zbtn export">新建</span>}
     </div>
 }
 
@@ -164,7 +166,7 @@ function search(ref, concat, cb) {
     })
 }
 
-function _doList(ref) {
+function init1(ref) {
     if (!ref.D) return
     let R
     const p = ref.props
@@ -218,11 +220,37 @@ function _doList(ref) {
             })
             ref.paths.sort()
         }
-        if (ref.isDev && p.diyColumn) ref.updateMeta("p.P.columns", ref.paths.map(k => { return { header: k, path: k } }))
     }
-    if (ref.isDev && p.searchFieldOn && !p.searchFields) {
+    if (!ref.isDev) return
+
+    function scroll(k) {
+        setTimeout(() => {
+            const el = parent.document.querySelector(".nodepe .pP" + k)
+            el.scrollIntoViewIfNeeded({ behavior: "smooth" })
+            el.style.color = "red"
+        }, 200)
+    }
+    if (p.diyColumn && !p.columns) {
+        ref.updateMeta("p.P.columns", ref.paths.map(k => { return { header: k, path: k } }))
+        scroll("columns")
+    }
+    if (p.enableSearchField && !p.searchFields) {
         ref.updateMeta("p.P.searchFields", [{ path: "", query: "包含" }])
-        parent.document.querySelector(".nodepe .zbtns").scrollIntoViewIfNeeded()
+        scroll("searchFields")
+    }
+    if (p.enableCRUD && !p.editPop) {
+        p.columns.push({ header: "操作", display: 'html(\'<span class="op">编辑</span><span class="op">删除</span>\')' })
+        ref.updateMeta("p.P.columns", p.columns)
+        ref.updateMeta("p.P.onCellClick", (p.onCellClick ? p.onCellClick + "; " : "") + 'exc($exp[text] || "")')
+        ref.updateMeta("joe.exp.删除", 'confirm("提示", "确定要删除吗？数据不可恢复！")\n$' + ref.D.model + '.delete($x._id)\n$r ? info("已删除") : warn("删除失败")\nrender()')
+        ref.updateMeta("joe.exp.编辑", '$(".zp141").edit($x)')
+        ref.updateMeta("p.P.onNew", '$' + ref.D.model + '.create("' + (ref.D.arr[0] ? ref.D.arr[0].type : "type") + '"' + (ref.D.model == "xdb" ? ', date().getTime() + ""' : "type") + ', $x.x); $r ? info("已新建") : warn("新建失败")')
+        ref.updateMeta("p.P.onSave", '$' + ref.D.model + '.modify($x._id, $x.x); $r ? info("已保存") : warn("保存失败")')
+        ref.updateMeta("p.P.editPop", ref.paths.filter(a => a.path).map((path, i) => { return { path, label: ref.heads[i], type: "单行文本" } }))
+        ref.paths = p.columns.map(a => a.path)
+        ref.heads = p.columns.map(a => a.header)
+        ref.display = p.columns.map(a => a.display)
+        scroll("editPop")
     }
 }
 
@@ -290,6 +318,7 @@ function switcher(ref, e) {
         query[T.fields[i + 1]] = T[parent][a]
         parent += "_" + a
     })
+    Object.keys(query).forEach(k => { if (query[k] == undefined) query[k] = { $exists: false } })
     distinct(ref, field, query, arr => T[path] = arr)
 }
 
@@ -307,6 +336,7 @@ function selNode(ref, e) {
         ref.Q[T.fields[i + 1]] = T[parent][a]
         parent += "_" + a
     })
+    Object.keys(ref.Q).forEach(k => { if (ref.Q[k] == undefined) ref.Q[k] = { $exists: false } })
     ref.O.skip = 0
     search(ref)
 }
@@ -377,9 +407,10 @@ function download(ref) {
 
 function onCellClick(ref, $ev) {
     let $el = getTD($ev.target)
+    if (!$el) return
     ref.data = ref.list[nthChild($el.parentElement)]
     let path = ref.paths[nthChild($el)]
-    excA(ref.props.onCellClick, { $el, $ev, path, $x: ref.data, value: getIn(ref.data, path), text: $ev.target.innerText, $exp: ref.ctx.$exp })
+    exc(ref.props.onCellClick, { $el, $ev, path, $x: ref.data, value: getIn(ref.data, path), text: $ev.target.innerText, $exp: ref.ctx.$exp })
 }
 
 /* --------------------------------------------------------------------------------------------- */
@@ -422,8 +453,8 @@ function contextMenu(ref, e) {
         ]
     } else { // TBODY
         ref.data = ref.list[nthChild($el.parentElement)]
-       let v = getIn(ref.data, k)
-        if (ref.props.onRightClick) excA(ref.props.onRightClick, { $el, $ev: e, path: k, $x: ref.data, value: v, text: e.target.innerText, $exp: ref.ctx.$exp })
+        let v = getIn(ref.data, k)
+        if (ref.props.onRightClick) exc(ref.props.onRightClick, { $el, $ev: e, path: k, $x: ref.data, value: v, text: e.target.innerText, $exp: ref.ctx.$exp })
         if (v != undefined && v != null) {
             arr = ["等于", "不等于"]
             if (typeof v == "number") arr = arr.concat(["大于或等于", "小于或等于"])
@@ -439,7 +470,7 @@ function contextMenu(ref, e) {
 }
 
 function getTD(el) {
-    return el.nodeName == "TD" || el.nodeName == "TH" ? el : getTD(el.parentElement)
+    if (el.nodeName != "TABLE") return el.nodeName == "TD" || el.nodeName == "TH" ? el : getTD(el.parentElement)
 }
 
 function nthChild(el) {
@@ -448,7 +479,66 @@ function nthChild(el) {
     return i
 }
 
+function toEdit(ref, $x) {
+    function get(o) {
+        return o.path.split(".").reduce((curr, k) => {
+            if (!curr) return
+            return curr[k]
+        }, O)
+    }
 
+    function setIn(o, path, v) {
+        if (path.length > 1) {
+            let p = path[0]
+            if (o[p] === undefined || typeof o[p] !== "object") o[p] = parseInt(path[1]).toString() == path[1] ? [] : {}
+            setIn(o[p], path.slice(1), v)
+        } else {
+            v === undefined ? delete o[path[0]] : o[path[0]] = v
+        }
+    }
+    const set = (o, v) => {
+        let path = o.path.split(".")
+        if (o.type == "单选") {
+            //
+        } else if (o.type == "多选") {
+            let V = get(o)
+            if (!Array.isArray(V)) V = []
+            V.indexOf(v) > -1 ? V.splice(V.indexOf(v), 1) : V.push(v)
+            v = V
+        } else if (o.type == "布尔值") {
+            v = v.target.checked
+        } else {
+            v = v.target.value
+            //  if (v == "") return setIn(O, path, undefined)
+            if (o.type == "数值") v = parseFloat(v)
+        }
+        setIn(O, path, v)
+    }
+
+    let O = $x ? JSON.parse(JSON.stringify($x)) : {}
+    const Type = {
+        单行文本: o => <input defaultValue={get(o)} onChange={e => set(o, e)} className="zinput"/>,
+        多行文本: o => <textarea defaultValue={get(o)} onChange={e => set(o, e)} className="zinput"/>,
+        数值: o => <input type="number" defaultValue={get(o)} onChange={e => set(o, e)} className="zinput"/>,
+        下拉选择: o => <select defaultValue={get(o)} onChange={e => set(o, e)} className="zinput">{(o.options || ["未配置选项列表"]).map((a, i) => <option value={a} key={i}>{a}</option>)}</select>,
+        单选: o => (o.options || ["未配置选项列表"]).map((a, i) => <label key={i}><input onChange={e => set(o, a)} type="radio" name={o.path} defaultChecked={get(o) == a}/>{a}</label>),
+        多选: o => (o.options || ["未配置选项列表"]).map((a, i) => <label key={i}><input onChange={e => set(o, a)} type="checkbox" name={o.path} defaultChecked={(get(o) || []).includes(a)}/>{a}</label>),
+        布尔值: o => <input type="checkbox" defaultChecked={!!get(o)} onChange={e => set(o, e)}/>,
+        日期: o => <input type="date" defaultValue={get(o)} onChange={e => set(o, e)} className="zinput"/>,
+        时间: o => <input type="time" step="1" defaultValue={get(o)} onChange={e => set(o, e)} className="zinput"/>,
+        日期时间: o => <input type="datetime-local" step="1" defaultValue={get(o) ? new Date(get(o)).format("YYYY-MM-DDTHH:MM:ss") : undefined} onChange={e => set(o, e)} className="zinput"/>,
+    }
+    ref.pop = [$x ? "编辑" : "新建", <div className="editpop">{ref.props.editPop.map((o, i) => <label key={i}><span className={o.required ? "zrequire" : ""}>{o.label || o.path}</span>{Type[o.type](o)}</label>)}</div>, <button className="zbtn zprimary" onClick={() => op()}>提交</button>]
+
+    function op() {
+        log(O, $x ? ref.props.onSave : ref.props.onNew)
+        exc($x ? ref.props.onSave : ref.props.onNew, { $x: O }, () => {
+            ref.pop = undefined
+            ref.render()
+        })
+    }
+    ref.render()
+}
 
 
 $plugin({
@@ -488,23 +578,23 @@ $plugin({
         type: "number",
         ph: "默认至少30条",
         label: "超过多少数据量才显示筛选",
-        show: 'p.P.filterTree'
+        show: "p.P.filterTree"
     }, {
         prop: "onSearch",
         type: "text",
         label: "搜索事件",
         ph: 'log(arr, count)',
-        show: 'p.P.popFilter || p.P.searchBox'
+        show: "p.P.popFilter || p.P.searchBox"
     }, {
         prop: "onCellClick",
         type: "text",
         label: "单元格点击事件",
-        ph: "log(path, text, $x)"
+        ph: "log($x, path, value, text, $ev, $el, $exp)"
     }, {
         prop: "onRightClick",
         type: "text",
         label: "单元格右键事件",
-        ph: "log(path, text, $x)"
+        ph: "log($x, path, value, text, $ev, $el, $exp)"
     }, {
         prop: "diyColumn",
         type: "switch",
@@ -513,7 +603,7 @@ $plugin({
         prop: "columns",
         type: "array",
         label: "列配置",
-        show: 'p.P.diyColumn',
+        show: "p.P.diyColumn",
         struct: [{
             prop: "header",
             type: "text",
@@ -529,14 +619,14 @@ $plugin({
             ph: "date($x).format()"
         }]
     }, {
-        prop: "searchFieldOn",
+        prop: "enableSearchField",
         type: "switch",
         label: "开启查询选项"
     }, {
         prop: "searchFields",
         type: "array",
         label: "查询选项",
-        show: 'p.P.searchFieldOn',
+        show: "p.P.enableSearchField",
         struct: [{
             prop: "label",
             type: "text",
@@ -554,7 +644,55 @@ $plugin({
         }, {
             prop: "options",
             type: "text",
-            label: "下拉选项",
+            label: "选项列表",
+            ph: '(["", "男", "女"])'
+        }]
+    }, {
+        prop: "enableCRUD",
+        type: "switch",
+        label: "开启【新建/删除/编辑】操作"
+    }, {
+        prop: "onNew",
+        type: "text",
+        label: "新建事件",
+        show: "p.P.enableCRUD"
+    }, {
+        prop: "onSave",
+        type: "text",
+        label: "保存事件",
+        show: "p.P.enableCRUD"
+    }, {
+        prop: "editPop",
+        type: "array",
+        label: "新建/编辑表单选项",
+        show: "p.P.enableCRUD",
+        struct: [{
+            prop: "label",
+            type: "text",
+            label: "表单标签",
+            ph: "为空时显示字段路径"
+        }, {
+            prop: "path",
+            type: "text",
+            label: "字段路径"
+        }, {
+            prop: "type",
+            type: "select",
+            label: "字段类型",
+            items: ["单行文本", "多行文本", "数值", "布尔值", "单选", "多选", "下拉选择", "日期", "时间", "日期时间"]
+        }, {
+            prop: "placeholder",
+            type: "text",
+            label: "占位符",
+            ph: 'placeholder'
+        }, {
+            prop: "required",
+            type: "switch",
+            label: "必填"
+        }, {
+            prop: "options",
+            type: "text",
+            label: "选项列表",
             ph: '(["", "男", "女"])'
         }]
     }],
